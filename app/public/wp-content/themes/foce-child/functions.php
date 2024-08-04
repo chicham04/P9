b6dddca0874845b9dcbba33e26c8266f4461f3c7
chicham04 <167074210+chicham04@users.noreply.github.com>
2024-08-04 15:16:48 +0200

    modif function

--- app/public/wp-content/themes/foce-child/functions.php
+++ app/public/wp-content/themes/foce-child/functions.php
@@ -1,8 +1,39 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    //wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/scss/style.css', array(), '1.0.0', 'all');
+
+    $file_name = '/scss/style.css'; // Nom du fichier CSS
+	$style_path =  get_stylesheet_directory() . $file_name; // Chemin vers votre fichier CSS
+	wp_enqueue_style(
+        'theme-style', // Identifiant unique pour votre style
+        get_stylesheet_directory_uri(). $file_name,
+        array(), // Dépendances, le cas échéant
+        file_exists($style_path) ? filemtime($style_path) : false // Version du fichier basée sur la date de dernière modification ( pour les probleme de cache)
+    );
 }
+
+// Import lib Swiper
+add_action("wp_enqueue_scripts", 'swiper');
+
+function swiper() {
+    wp_enqueue_style("swiper-css", "https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css");
+    wp_enqueue_script("swiper-js", "https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js");
+}
+
+add_action('wp_enqueue_scripts', 'customScripts');
+
+function customScripts() {
+    wp_enqueue_script("Title", get_stylesheet_directory_uri() . '/js/title.js', array(), '1.0.0', true);
+    wp_enqueue_script("Float", get_stylesheet_directory_uri()."/js/float.js", array(), '1.0.0', true);
+    wp_enqueue_script("Parallaxe", get_stylesheet_directory_uri()."/js/parallaxe.js", array(), '1.0.0', true);
+    wp_enqueue_script("Menu-burger", get_stylesheet_directory_uri()."/js/menu-burger.js", array('jquery'), '1.0.0', true);
+}
+
+
+
 
 // Get customizer options form parent theme
 if ( get_stylesheet() !== get_template() ) {
