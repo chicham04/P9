f890f7f0ecac47065da71f314cc98402dc134252
chicham04 <167074210+chicham04@users.noreply.github.com>
2024-08-04 15:46:30 +0200

    float

---
+++ app/public/wp-content/themes/foce-child/js/float.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let element = document.querySelector(".banner__parallaxe-logo");
+  
+    setTimeout(function () {
+      element.classList.add("float");
+    }, 4000);
+  });
+  
